719800e32e2dee9d58b0f89605d2759e9bad8f8f
Soumyaditya Choudhuri <soum-c@users.noreply.github.com>
2018-04-09 00:03:25 +0800

    Corrected operands on line 6 and 14 to "*="
    Previously added the extra operand which should have been multiplied.
    
    Works on problem: exponentiate on dunjudge.me

--- math/cpp/qexplonglong.cpp
+++ math/cpp/qexplonglong.cpp
@@ -3,7 +3,7 @@ long long qmult(long long X, long long Y, long long M) {
     long long half = qmult(X, Y/2, M);
     half += half;
     half %= M;
-    if (Y % 2==1) half += half;
+    if (Y % 2==1) half *= half;
     return half%=M;
 }
 long long qexp(long long A, long long B, long long M) {
@@ -11,6 +11,6 @@ long long qexp(long long A, long long B, long long M) {
     long long half = qexp(A, B/2, M); //Note that B/2 is rounded down, this will be compensated later
     half = qmult(half, half, M); //qmult is required to ensure this operation does not overflow when M is close to 2^62
     half %= M;
-    if (B%2==1) half += A%M;  //Compensate the 'round down' of B/2 when B is odd
+    if (B%2==1) half *= A%M;  //Compensate the 'round down' of B/2 when B is odd
     return half%M;
 }
